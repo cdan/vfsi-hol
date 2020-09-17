b0fd9b4126632d487a450d11670fb9387474fcf4
cdan <ssbdan@gmail.com>
2020-09-17 11:54:47 +0800

    Create nimbus_vcenter70_u1.rb

---
+++ nimbus_vcenter70_u1.rb
@@ -0,0 +1,49 @@
+oneGB = 1 * 1000 * 1000 # in KB
+ 
+$testbed = Proc.new do
+  {
+    "name" => "testbed-test",
+    "version" => 3,
+    "esx" => (0..0).map do | idx |
+      {
+        "name" => "esx.#{idx}",
+        "vc" => "vc.0",
+        "customBuild" => "ob-16850804",
+        "dc" => "vcqaDC",
+        "clusterName" => "cluster0",
+        "style" => "fullInstall",
+        "cpus" => 32, # 32 vCPUs
+        "memory" => 98000, # 98GB memory
+        "fullClone" => true,
+        "disks" => [ 2 * 1000 * oneGB ], # 2 TB Disk
+        "guestOSlist" => [         
+          {
+            "vmName" => "centos-vm.#{idx}",
+            "ovfuri" => NimbusUtils.get_absolute_ovf("CentOS-7-64-VMTools/CentOS-7-x64.ovf")
+          }
+        ]
+      }
+    end,
+ 
+    "vcs" => [
+      {
+        "name" => "vc.0",
+        "type" => "vcva",
+        "customBuild" => "ob-16860138",
+        "dcName" => ["vcqaDC"],
+        "enableDrs" => true,
+        "clusters" => [
+          {
+            "name" => "cluster0",
+            "dc" => "vcqaDC"
+          }
+        ]
+      }
+    ],
+ 
+    "beforePostBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
+    end,
+    "postBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
+    end
+  }
+end
